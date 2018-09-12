4dc70f9fae0a751cbd03611160e085fd4fc4d1d9
leto <1907984744@qq.com>
2018-09-12 21:24:34 +0800

    引入weui，更正日期逻辑

--- src/api/configure.js
+++ src/api/configure.js
@@ -1,15 +1,38 @@
 const apiRoot = 'https://dav.jianguoyun.com/dav/';
 const fileName = '我的坚果云/JL日历数据-请勿删除.txt';
-const testFileName = '我的坚果云/__JL日历临时文件-可删除'
+const testFileName = '我的坚果云/__JL日历临时文件-可删除';
 const dbKeys = {
   jgyAccount: 'jgyAccount',
   jgyPassword: 'jgyPassword',
   record: 'record'
 };
+const colors = {
+  currentChosen: {
+    color: 'white',
+    back: '#58cc69'
+  },
+  today: {
+    color: 'white',
+    back: '#ccc'
+  },
+  normalDay: {
+    color: '#a18ada',
+    back: 'transparent'
+  },
+  sunday: {
+    color: '#f488cd',
+    back: 'transparent'
+  }
+};
+const chosenDayToken = '__isCurrentChosenDayHighlight';
+const timesArray = ['0 😘', '1 😑', '2 ☹️', '3 😣', '4 😵', '>=5 😱'];
 
 module.exports = {
   apiRoot,
   fileName,
   dbKeys,
-  testFileName
+  testFileName,
+  colors,
+  timesArray,
+  chosenDayToken
 };
