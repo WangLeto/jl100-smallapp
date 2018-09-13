08ff3120e9fecf449d925b5e728ec681f99ea5ad
leto <1907984744@qq.com>
2018-09-13 08:04:47 +0800

    引入rainbow.js

--- src/utils/tools.js
+++ src/utils/tools.js
@@ -1,13 +1,21 @@
 import _ from 'lodash';
+import Rainbow from 'rainbowvis.js';
 
 const sleep = async function(seconds) {
   await setTimeout(() => {
   }, 1000 * seconds);
 };
 
+const getRainbowColor = function(color1, color2, colorsNum, index) {
+  const rainbow = new Rainbow();
+  rainbow.setSpectrum(color1, color2);
+  return rainbow.colorAt(index / colorsNum * 100);
+};
+
 const mountLodash = () => {
   let des = {
-    sleep: sleep
+    sleep: sleep,
+    getRainbowColor: getRainbowColor
   };
   Object.assign(_, des);
 };
