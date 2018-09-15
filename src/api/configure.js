450d641280d15e0d103e35dbd80b333bb4c95875
leto <1907984744@qq.com>
2018-09-15 18:56:51 +0800

    内部名称显示修改、折线图拖动优化

--- src/api/configure.js
+++ src/api/configure.js
@@ -1,6 +1,6 @@
 const apiRoot = 'https://dav.jianguoyun.com/dav/';
-const fileName = '我的坚果云/JL日历数据-请勿删除.txt';
-const testFileName = '我的坚果云/__JL日历临时文件-可删除';
+const fileName = '我的坚果云/不良日历数据-请勿删除.txt';
+const testFileName = '我的坚果云/__不良日历临时文件-可删除';
 const dbKeys = {
   jgyAccount: 'jgyAccount',
   jgyPassword: 'jgyPassword',
